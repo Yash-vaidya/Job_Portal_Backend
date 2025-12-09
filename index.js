5a335dd57b5359ef2fd82bf2ab7086daca4db95c
Yash Vaidya <yashvaidya9623@gmail.com>
2025-12-09 17:08:52 +0530

    fixed cors issue

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// };
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://job-portal-frontend-jhnz.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(cors(corsOptions));
 
